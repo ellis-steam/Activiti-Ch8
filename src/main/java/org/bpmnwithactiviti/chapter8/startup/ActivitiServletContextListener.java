abdfda9b78dc6c1589f46c1d35c13dbf7cb9a999
Ellis <ellis@s-team.com.tw>
2016-07-26 10:09:29 +0800

    Firt commit

---
+++ src/main/java/org/bpmnwithactiviti/chapter8/startup/ActivitiServletContextListener.java
@@ -0,0 +1,20 @@
+package org.bpmnwithactiviti.chapter8.startup;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ActivitiServletContextListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
